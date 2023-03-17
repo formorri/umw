bc469d32a2e563cf1d52cfb47d2f524d492d19fa
User <user@Users-iMac.local>
2023-03-17 20:05:23 +0800

    firts commit

---
+++ next.config.js
@@ -0,0 +1,31 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
+const path = require('path')
+const basePath = process.env.NODE_ENV === 'production' ? '/development/UMW' : '';
+
+module.exports = {
+  trailingSlash: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  basePath,
+  assetPrefix: basePath,
+  images: {
+    path: `${basePath}/_next/image`,
+    unoptimized: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // This will replace all `basePath` occurrences with an empty string
+      config.output.publicPath = config.output.publicPath.replace(basePath, '');
+    }
+    return config;
+  },
+};
+
+
