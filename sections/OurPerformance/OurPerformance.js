f01b27e927c9adb426acc78644e9016013b5af30
User <user@Users-iMac.local>
2023-04-11 18:51:29 +0800

    cleaned up some css

--- sections/OurPerformance/OurPerformance.js
+++ sections/OurPerformance/OurPerformance.js
@@ -14,7 +14,7 @@ const tabData = [
     gif: "automotive.gif",
     iconText: "automotive",
     color: "#C65A81",
-    inactive: "#DAB9C5",
+    inactive: "#E0D5D9",
     switcher: [
       {
         key: 0,
@@ -78,7 +78,7 @@ const tabData = [
     gif: "equipment.gif",
     iconText: "equipment",
     color: "#F2A818",
-    inactive: "#E7D1AD",
+    inactive: "#E5DDD1",
     backgroundColor: "#FBF0D3",
     title: "equipment",
     // category: "equipment",
@@ -107,7 +107,7 @@ const tabData = [
     gif: "mnE.gif",
     iconText: "manufacturing & engineering",
     color: "#4B4CA7",
-    inactive: "#B5B6D0",
+    inactive: "#D3D4DD",
     backgroundColor: "#EFE8F3",
     title: "MANUFACTURING & ENGINEERING",
     // category: "m & e",
@@ -136,7 +136,7 @@ const tabData = [
     gif: "aerospace.gif",
     iconText: "aerospace",
     color: "#378DDB",
-    inactive: "#B0CAE0",
+    inactive: "#D2DBE2",
     backgroundColor: "#E4EFF9",
     title: "aerospace",
     // category: "aerospace",
@@ -167,7 +167,7 @@ const tabData = [
     gif: "development.gif",
     iconText: "development",
     color: "#9E6D2B",
-    inactive: "#CEBFAD",
+    inactive: "#DCD7D1",
     backgroundColor: "#E1D5C8",
     title: "development",
     // category: "development",
@@ -265,11 +265,11 @@ const OurPerformance = () => {
       }}
       transition={{ duration: 1 }}
     >
-      <div id='our-performance' className={styles.wrapper}>
+      <div id="our-performance" className={styles.wrapper}>
         <h2 className={styles.title}>Our Performance</h2>
         {/* tabs */}
         <div className={styles["tab-container"]}>
-        <div className={styles["instructions"]}>
+          <div className={styles["instructions"]}>
             <div className={styles["indicator-wrapper"]}>
               <Image
                 src={images.touch}
@@ -412,56 +412,57 @@ const OurPerformance = () => {
                     >
                       {tabDesc}
                     </p>
-                    <div className={styles['card-container']}>
-
-                    {tabDataItem &&
-                      tabDataItem.map((dataItem) => (
-                        <div
-                          className={styles.card}
-                          key={dataItem.key}
-                          // style={{
-                          //   background: `linear-gradient(to right, transparent, ${tabInactive})`,
-                          // }}
-                        >
-                          <p
-                            className={styles["data-title"]}
-                            style={{ color: tabColor }}
+                    <div className={styles["card-container"]}>
+                      {tabDataItem &&
+                        tabDataItem.map((dataItem) => (
+                          <div
+                            className={styles.card}
+                            key={dataItem.key}
+                            // style={{
+                            //   background: `linear-gradient(to right, transparent, ${tabInactive})`,
+                            // }}
                           >
-                            {dataItem.title}
-                          </p>
-                          <p
-                            className={styles["data-units"]}
-                            style={{
-                              "--tab-color": tabColor,
-                            }}
-                          >
-                            {dataItem.units}
-                          </p>
-                          {/* <p className={styles["data-figure"]}>
+                            <p
+                              className={styles["data-title"]}
+                              style={{ color: tabColor }}
+                            >
+                              {dataItem.title}
+                            </p>
+                            <p
+                              className={styles["data-units"]}
+                              style={{
+                                "--tab-color": tabColor,
+                              }}
+                            >
+                              {dataItem.units}
+                            </p>
+                            {/* <p className={styles["data-figure"]}>
                           {dataItem.figure}
                         </p> */}
-                          <CountUp
-                            start={0}
-                            end={dataItem.figure}
-                            delay={0}
-                            decimal="."
-                            decimals={1}
-                            enableScrollSpy={true}
-                            scrollSpyDelay={1}
-                            scrollSpyOnce={true}
-                          >
-                            {({ countUpRef }) => (
-                              <div>
-                                <p
-                                  className={styles["data-figure"]}
-                                  ref={countUpRef}
-                                ></p>
-                              </div>
-                            )}
-                          </CountUp>
-                          <p className={styles["data-year"]}>{dataItem.year}</p>
-                        </div>
-                      ))}
+                            <CountUp
+                              start={0}
+                              end={dataItem.figure}
+                              delay={0}
+                              decimal="."
+                              decimals={1}
+                              enableScrollSpy={true}
+                              scrollSpyDelay={1}
+                              scrollSpyOnce={true}
+                            >
+                              {({ countUpRef }) => (
+                                <div>
+                                  <p
+                                    className={styles["data-figure"]}
+                                    ref={countUpRef}
+                                  ></p>
+                                </div>
+                              )}
+                            </CountUp>
+                            <p className={styles["data-year"]}>
+                              {dataItem.year}
+                            </p>
+                          </div>
+                        ))}
                     </div>
                     <Button1
                       link="pdf/our-performance.pdf"
@@ -544,10 +545,39 @@ const OurPerformance = () => {
                       backgroundColor:
                         activeSwitch === 0 ? "#C65A81" : "#F7E8EE",
 
-                      color: activeSwitch === 0 ? "#F7E8EE" : "#D39FB2",
+                      color: activeSwitch === 0 ? "#F7E8EE" : "#DCC6CE",
                     }}
                   >
                     <p>UMW Toyota</p>
+                    <div className={styles["indicator-switcher"]}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="15.997"
+                        height="24.833"
+                        viewBox="0 0 15.997 24.833"
+                        className={styles.indicator}
+                      >
+                        <g
+                          id="Group_6180"
+                          data-name="Group 6180"
+                          transform="translate(-134.051 439.644)"
+                        >
+                          <path
+                            id="Path_12036"
+                            data-name="Path 12036"
+                            d="M134.051-385.468c.036-.131.068-.264.11-.394a2.187,2.187,0,0,1,2.581-1.437l.215.051v-.276c0-1.665-.008-3.33,0-4.995a2.176,2.176,0,0,1,2.162-2.163,2.172,2.172,0,0,1,2.19,2.132c.016,1.188,0,2.376,0,3.564v.264c1.6-.175,2.274.179,2.844,1.5a2.185,2.185,0,0,1,1.751.085,2.165,2.165,0,0,1,1.118,1.293,7.756,7.756,0,0,1,1.234-.012,2.084,2.084,0,0,1,1.759,1.974c.026,1.267.051,2.541-.038,3.8a7.984,7.984,0,0,1-6.171,7.11c-.392.087-.792.136-1.189.2h-1.163c-.341-.054-.688-.085-1.023-.166a7.922,7.922,0,0,1-6.091-5.742,14.485,14.485,0,0,1-.3-1.561Zm11.627,2.462c0-.721,0-1.423,0-2.124a.731.731,0,0,0-.727-.774.731.731,0,0,0-.725.775c0,.622,0,1.244,0,1.867v.255h-1.453v-.272c0-1.091,0-2.182,0-3.273a.767.767,0,0,0-.238-.611.678.678,0,0,0-.763-.139.714.714,0,0,0-.453.708q0,1.661,0,3.321v.26h-1.453v-.305q0-4.485,0-8.97c0-.081,0-.162,0-.242a.734.734,0,0,0-.681-.7.731.731,0,0,0-.755.585,1.6,1.6,0,0,0-.015.314q0,5.976,0,11.952v.278h-1.453v-.272q0-2.352,0-4.7a.79.79,0,0,0-.239-.635.678.678,0,0,0-.764-.138.714.714,0,0,0-.452.709c0,1.511,0,3.022.005,4.534a4.94,4.94,0,0,0,.06.724,6.549,6.549,0,0,0,7.6,5.565,6.534,6.534,0,0,0,5.362-5.76c.092-1.222.05-2.454.052-3.682a.712.712,0,0,0-.731-.692.728.728,0,0,0-.714.712c-.009.24,0,.48,0,.731Z"
+                            transform="translate(0 -42.049)"
+                            fill={activeSwitch === 0 ? "#F7E8EE" : "#DCC6CE"}
+                          />
+                          <path
+                            id="Path_12037"
+                            data-name="Path 12037"
+                            d="M134.051-435.034c.066-.306.115-.617.2-.918a5.1,5.1,0,0,1,5.211-3.681,5.108,5.108,0,0,1,4.7,4.313c.037.245.046.494.07.757h-1.443a3.684,3.684,0,0,0-3.818-3.622,3.677,3.677,0,0,0-3.483,3.636h-1.441Z"
+                            fill={activeSwitch === 0 ? "#F7E8EE" : "#DCC6CE"}
+                          />
+                        </g>
+                      </svg>
+                    </div>
                   </div>
                   <div
                     className={styles["inner-tab-content"]}
@@ -556,12 +586,41 @@ const OurPerformance = () => {
                     }}
                     style={{
                       backgroundColor:
-                        activeSwitch === 0 ? "#B7C6BE" : "#07522A",
+                        activeSwitch === 0 ? "#D2D6D3" : "#07522A",
 
-                      color: activeSwitch === 0 ? "#779A87" : "#B7C6BE",
+                      color: activeSwitch === 0 ? "#B5C3BA" : "#B7C6BE",
                     }}
                   >
                     <p>Perodua</p>
+                    <div className={styles["indicator-switcher"]}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="15.997"
+                        height="24.833"
+                        viewBox="0 0 15.997 24.833"
+                        className={styles.indicator}
+                      >
+                        <g
+                          id="Group_6180"
+                          data-name="Group 6180"
+                          transform="translate(-134.051 439.644)"
+                        >
+                          <path
+                            id="Path_12036"
+                            data-name="Path 12036"
+                            d="M134.051-385.468c.036-.131.068-.264.11-.394a2.187,2.187,0,0,1,2.581-1.437l.215.051v-.276c0-1.665-.008-3.33,0-4.995a2.176,2.176,0,0,1,2.162-2.163,2.172,2.172,0,0,1,2.19,2.132c.016,1.188,0,2.376,0,3.564v.264c1.6-.175,2.274.179,2.844,1.5a2.185,2.185,0,0,1,1.751.085,2.165,2.165,0,0,1,1.118,1.293,7.756,7.756,0,0,1,1.234-.012,2.084,2.084,0,0,1,1.759,1.974c.026,1.267.051,2.541-.038,3.8a7.984,7.984,0,0,1-6.171,7.11c-.392.087-.792.136-1.189.2h-1.163c-.341-.054-.688-.085-1.023-.166a7.922,7.922,0,0,1-6.091-5.742,14.485,14.485,0,0,1-.3-1.561Zm11.627,2.462c0-.721,0-1.423,0-2.124a.731.731,0,0,0-.727-.774.731.731,0,0,0-.725.775c0,.622,0,1.244,0,1.867v.255h-1.453v-.272c0-1.091,0-2.182,0-3.273a.767.767,0,0,0-.238-.611.678.678,0,0,0-.763-.139.714.714,0,0,0-.453.708q0,1.661,0,3.321v.26h-1.453v-.305q0-4.485,0-8.97c0-.081,0-.162,0-.242a.734.734,0,0,0-.681-.7.731.731,0,0,0-.755.585,1.6,1.6,0,0,0-.015.314q0,5.976,0,11.952v.278h-1.453v-.272q0-2.352,0-4.7a.79.79,0,0,0-.239-.635.678.678,0,0,0-.764-.138.714.714,0,0,0-.452.709c0,1.511,0,3.022.005,4.534a4.94,4.94,0,0,0,.06.724,6.549,6.549,0,0,0,7.6,5.565,6.534,6.534,0,0,0,5.362-5.76c.092-1.222.05-2.454.052-3.682a.712.712,0,0,0-.731-.692.728.728,0,0,0-.714.712c-.009.24,0,.48,0,.731Z"
+                            transform="translate(0 -42.049)"
+                            fill={activeSwitch === 0 ? "#B5C3BA" : "#B7C6BE"}
+                          />
+                          <path
+                            id="Path_12037"
+                            data-name="Path 12037"
+                            d="M134.051-435.034c.066-.306.115-.617.2-.918a5.1,5.1,0,0,1,5.211-3.681,5.108,5.108,0,0,1,4.7,4.313c.037.245.046.494.07.757h-1.443a3.684,3.684,0,0,0-3.818-3.622,3.677,3.677,0,0,0-3.483,3.636h-1.441Z"
+                            fill={activeSwitch === 0 ? "#B5C3BA" : "#B7C6BE"}
+                          />
+                        </g>
+                      </svg>
+                    </div>
                   </div>
                 </div>
                 {/* <div
@@ -577,7 +636,7 @@ const OurPerformance = () => {
                     {switcherTitle}
                   </h3>
                 </div> */}
-                <div className={styles["instructions"]}>
+                {/* <div className={styles["instructions"]}>
                   <div className={styles["indicator-wrapper"]}>
                     <Image
                       src={images.touch}
@@ -587,7 +646,7 @@ const OurPerformance = () => {
                     />
                   </div>
                   <p className={styles["indicator-text"]}>tap to switch tabs</p>
-                </div>
+                </div> */}
                 <div className={styles["content-data-container"]}>
                   <p
                     className={styles.category}
@@ -606,52 +665,54 @@ const OurPerformance = () => {
                     >
                       {switcherDesc}
                     </p>
-                    <div className={styles['card-container']}>
-                    {switcherDataItem &&
-                      switcherDataItem.map((dataItem) => (
-                        <div
-                          className={styles.card}
-                          key={dataItem.key}
-                          // style={{
-                          //   background: `linear-gradient(to right, transparent, ${switcherColorFaded})`,
-                          // }}
-                        >
-                          <p
-                            className={styles["data-title"]}
-                            style={{ color: switcherColor }}
+                    <div className={styles["card-container"]}>
+                      {switcherDataItem &&
+                        switcherDataItem.map((dataItem) => (
+                          <div
+                            className={styles.card}
+                            key={dataItem.key}
+                            // style={{
+                            //   background: `linear-gradient(to right, transparent, ${switcherColorFaded})`,
+                            // }}
                           >
-                            {dataItem.title}
-                          </p>
-                          <p
-                            className={styles["data-units"]}
-                            style={{
-                              "--tab-color": switcherColor,
-                            }}
-                          >
-                            {dataItem.units}
-                          </p>
-                          <CountUp
-                            start={0}
-                            end={dataItem.figure}
-                            delay={0}
-                            decimal="."
-                            decimals={1}
-                            enableScrollSpy={true}
-                            scrollSpyDelay={1}
-                            scrollSpyOnce={true}
-                          >
-                            {({ countUpRef }) => (
-                              <div>
-                                <p
-                                  className={styles["data-figure"]}
-                                  ref={countUpRef}
-                                ></p>
-                              </div>
-                            )}
-                          </CountUp>
-                          <p className={styles["data-year"]}>{dataItem.year}</p>
-                        </div>
-                      ))}
+                            <p
+                              className={styles["data-title"]}
+                              style={{ color: switcherColor }}
+                            >
+                              {dataItem.title}
+                            </p>
+                            <p
+                              className={styles["data-units"]}
+                              style={{
+                                "--tab-color": switcherColor,
+                              }}
+                            >
+                              {dataItem.units}
+                            </p>
+                            <CountUp
+                              start={0}
+                              end={dataItem.figure}
+                              delay={0}
+                              decimal="."
+                              decimals={1}
+                              enableScrollSpy={true}
+                              scrollSpyDelay={1}
+                              scrollSpyOnce={true}
+                            >
+                              {({ countUpRef }) => (
+                                <div>
+                                  <p
+                                    className={styles["data-figure"]}
+                                    ref={countUpRef}
+                                  ></p>
+                                </div>
+                              )}
+                            </CountUp>
+                            <p className={styles["data-year"]}>
+                              {dataItem.year}
+                            </p>
+                          </div>
+                        ))}
                     </div>
                     <Button1
                       link="pdf/our-performance.pdf"
